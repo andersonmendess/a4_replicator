497ac16acbb2ec14b65284edd028271da58877d7
andersonmendess <anderson2000ano@gmail.com>
2021-03-19 23:44:26 -0300

    update base url

--- doc/web/flutter_service_worker.js
+++ doc/web/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "main.dart.js": "934b00ec9f0d14c2786e4e73c475310d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0b2af1eeebd5da07b388484bef3eea6b",
-"/": "0b2af1eeebd5da07b388484bef3eea6b"
+"index.html": "1c8811b2305db94a1c87dc3479bb753b",
+"/": "1c8811b2305db94a1c87dc3479bb753b"
 };
 
 // The application shell files that are downloaded before a service worker can
